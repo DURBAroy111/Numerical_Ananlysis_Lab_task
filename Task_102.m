5246f294cc808494d7c9cf873d224e76531ae440
DURBAroy111 <120655973+DURBAroy111@users.noreply.github.com>
2023-11-09 22:36:18 +0600

    Update Task_102.m

--- Task_102.m
+++ Task_102.m
@@ -2,19 +2,41 @@ f = @(x) x^3 - 5*x - 2;
 a = 2;
 b = 3;
 n = 35;
+roots = [];
+tolerance = 1e-6;
+
+
 if f(a) * f(b) <0
  for i=1:n
    c= (a+b)/2;
     fprintf('C%d = %.4f\n',i,c)
-   if f(a)*f(c)<0
+     roots = [roots; c];
+     if abs(f(c)) < tolerance
+        break;
+    elseif f(a)*f(c)<0
      b= c;
 
-   elseif
-     f(b)*f(c)<0
+   else
      a = c;
-   endif
-  endfor
+   end
+  end
 else
   disp('No root is present between these numbers');
   end;
+
+% Plot the function and roots for visualization
+x = linspace(a, b, 100);
+y = f(x);
+
+figure;
+plot(x, y,'k','LineWidth', 2); % Plot the function
+hold on;
+plot(roots, f(roots), 'ro', 'MarkerSize', 10); % Plot roots
+xlabel('Variable x');
+ylabel('Variable y');
+title('Bisection Method: Finding Roots');
+legend('Function', 'Roots');
+grid on;
+hold off;
+
 
