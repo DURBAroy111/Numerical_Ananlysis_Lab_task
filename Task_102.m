949ffcd5857b007150b85427648cb2e26909415c
DURBAroy111 <120655973+DURBAroy111@users.noreply.github.com>
2023-11-09 18:12:56 +0600

    Create Task_102.m

---
+++ Task_102.m
@@ -0,0 +1,20 @@
+f = @(x) x^3 - 5*x - 2;
+a = 2;
+b = 3;
+n = 35;
+if f(a) * f(b) <0
+ for i=1:n
+   c= (a+b)/2;
+   disp(c);
+   if f(a)*f(c)<0
+     b= c;
+
+   elseif
+     f(b)*f(c)<0
+     a = c;
+   endif
+  endfor
+else
+  disp('No root is present between these numbers');
+  end;
+
