fa22ebe86f08433ba92650e8daf9a8dbdeddb722
DURBAroy111 <120655973+DURBAroy111@users.noreply.github.com>
2023-11-09 17:22:41 +0600

    Create Task_101.m

---
+++ Task_101.m
@@ -0,0 +1,3 @@
+A= input('Enter the elements of first matrix inside a third bracket as space between the elements of first row follwed by a semicolon then the elements of 2nd row and so on like ([2 3 ; 4 5]): ')
+B= input ('Enter the elements of second matrix just like the first one: ')
+
